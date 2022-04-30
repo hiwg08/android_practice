c7b6b9b47e3a014fd371fea1af2e74b6ea2433c5
hiwg08 <hiwg08@gmail.com>
2022-04-30 22:59:02 +0900

    sixth commit

--- Real_Project/Assets/Script/Hima_Script/SolGryn.cs
+++ Real_Project/Assets/Script/Hima_Script/SolGryn.cs
@@ -7,6 +7,9 @@ public class SolGryn : MonoBehaviour
     // Start is called before the first frame update
 
     // Update is called once per frame
+
+    [SerializeField]
+    GameObject W1;
     void Update()
     {
         
@@ -34,6 +37,7 @@ public class SolGryn : MonoBehaviour
             yield return null;
         }
         temp = 0.4f;
+        StartCoroutine(W1_Go());
         cameraShake.Shake();
         while (transform.position.x >= -7 && transform.position.y <= 3)
         {
@@ -42,6 +46,7 @@ public class SolGryn : MonoBehaviour
             yield return null;
         }
         temp = 0.4f;
+        StartCoroutine(W1_Go());
         cameraShake.Shake();
         while (transform.position.y >= -3)
         {
@@ -50,6 +55,7 @@ public class SolGryn : MonoBehaviour
             yield return null;
         }
         temp = .4f;
+        StartCoroutine(W1_Go());
         cameraShake.Shake();
         while (transform.position.x <= 7 && transform.position.y <= 3)
         {
@@ -57,6 +63,22 @@ public class SolGryn : MonoBehaviour
             temp += 0.01f;
             yield return null;
         }
+        StartCoroutine(W1_Go());
         cameraShake.Shake();
+    }
+    IEnumerator W1_Go()
+    {
+        int count = 20;
+        float intervalAngle = 360 / count;
+        for (int i = 1; i <= count; i++)
+        {
+            GameObject clone = Instantiate(W1, transform.position, Quaternion.identity);
+            float angle = intervalAngle * i;
+            float x = Mathf.Cos(angle * Mathf.PI / 180.0f);
+            float y = Mathf.Sin(angle * Mathf.PI / 180.0f);
+            clone.GetComponent<Movement2D>().MoveSpeed = 8;
+            clone.GetComponent<Movement2D>().MoveTo(new Vector3(x, y));
+            yield return null;
+        }
     }
 }
