c38e5f6f262782b5432708981305c9fe08def7d1
hiwg08 <hiwg08@gmail.com>
2022-06-20 15:26:29 +0900

    [DOCS] 파일 삭제

--- Don't Get Underrated/Assets/Script/Common_Script/Life/Life_Of_Basic.cs
+++
@@ -1,9 +0,0 @@
-public interface Life_Of_Basic
-{
-    void OnDie(); // 모든 생명은 죽기 마련
-
-    void TakeDamage(int damage); // 목숨이 개수로 존재할 때 사용
-
-    void TakeDamage(float damage); // 목숨이 게이지로 존재할 때 사용
-
-}
